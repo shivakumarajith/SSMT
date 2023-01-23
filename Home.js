617521ace13fdbf5060aad148bd6b593556561f3
Ajith <aj.shivakumar123@gmail.com>
2023-01-23 14:13:16 +0530

    first commit

--- Home.js
+++ Home.js
@@ -1,15 +1,2 @@
 
-    function sendEmail() {
-      Email.send({
-        Host: "smtp.gmail.com",
-        Username: 'aj.shivakumar123@gmail.com',
-        Password: "lkfnvfxtkyejgagc",
-        To: 'aj.shivakumar123@gmail.com',
-        From: 'aj.shivakumar123@gmail.com',
-        Subject: "message from site",
-        Body: 'hii',
-      })
-        .then(function (message) {
-          alert("mail sent successfully")
-        });
-    }
+    
